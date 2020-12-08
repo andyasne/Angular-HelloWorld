f0cb0641b567058effe0033753a694a9993e30b2
johnnyaby123 <yohannesabayneh548@gmail.com>
2020-12-08 11:31:49 +0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/angular-helloworld'));
+app.use(express.static(__dirname + '/dist/angular-hello-world'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/angular-helloworld/index.html'));});
+'/dist/angular-hello-world/index.html'));});
 app.listen(process.env.PORT || 8080);
